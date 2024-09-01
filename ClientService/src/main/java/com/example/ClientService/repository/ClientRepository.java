d8bff3a0db0e4ac358504a04d02a4cf4ad93dd7f
ajithms01 <ajithms8866@gmail.com>
2024-09-01 10:49:24 +0530

    COMMIT

--- ClientService/src/main/java/com/example/ClientService/repository/ClientRepository.java
+++ ClientService/src/main/java/com/example/ClientService/repository/ClientRepository.java
@@ -8,11 +8,8 @@ import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client,Long> {
-<<<<<<< HEAD
     Optional<Client> findByUserNameAndPassword(String userName, String password);
-=======
-    Optional<Client> findByUserNameAndPassword(String username, String password);
->>>>>>> 25f0b976dbf84f4c117f3f6f06598f55f555d68c
+
 
     Optional<Client> findByName(String name);
 }
